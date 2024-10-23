d1fb169381610367c7cab2b499ecbe631f294ead
Wulferis <acer1171090@gmail.com>
2024-10-23 17:09:59 +1100

    Initial Port of SimpleUV

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Prowl.Unwrapper
- A simple c# UV Unwrapper used by the Prowl Game Engine.
+A port of SimpleUV https://github.com/huxingyi/simpleuv to c#
+
+This is a very rough port and has not be tested at all. I have no idea if it works or not yet.
+There is a TestScript.cs file that I used to run some simple tests, they seem to work.
+But I have not tested it with any real data yet nor have I tried to compare the results with the original library.
---
+++ Unwrapper/MeshDataTypes.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+
+namespace Prowl.Unwrapper
+{
+    public struct Vertex
+    {
+        public Vector3 position;
+    }
+
+    public struct UvRect
+    {
+        public float left;
+        public float top;
+        public float width;
+        public float height;
+
+        public readonly Vector2 Position => new(left, top);
+        public readonly Vector2 Size => new(width, height);
+    }
+
+    public struct Face
+    {
+        public int[] indices;
+
+        public Face() => indices = new int[3];
+    }
+
+    public struct TextureCoord
+    {
+        public float[] uv;
+        public TextureCoord() => uv = new float[2];
+        public TextureCoord(float u, float v) => uv = [u, v];
+    }
+
+    public struct FaceTextureCoords
+    {
+        public TextureCoord[] coords;
+        public FaceTextureCoords() => coords = new TextureCoord[3];
+    }
+
+    public class UVMesh
+    {
+        public List<Vertex> vertices = [];
+        public List<Face> faces = [];
+        public List<Vector3> faceNormals = [];
+        public List<int> facePartitions = [];
+    }
+}
